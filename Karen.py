dc2b8df7588aadf830023626d316635f2a73cbbd
SergioCollIbars <79155439+SergioCollIbars@users.noreply.github.com>
2021-02-23 18:40:37 +0100

    Add files via upload

--- Karen.py
+++ Karen.py
@@ -1,5 +1,5 @@
 #   --------------------------------    #
-#           KAREN AI                   #
+#           KAREN AI                    #
 #   --------------------------------    #
 
 #   Libraries
@@ -41,7 +41,7 @@ def takecommand(listener):
     with sr.Microphone() as source:
         print("listening...")
 
-        command = listener.listen(source, phrase_time_limit=5)
+        command = listener.listen(source, phrase_time_limit=10)
 
     try:
         query = listener.recognize_google(command, language='es-ES')  # Spanish language configured
@@ -50,6 +50,20 @@ def takecommand(listener):
     except Exception as e:
         print("Say that again please...")
         speak("Lo siento, no le he entendido. Pruebe a llamarme mas tarde")
+        return "None"
+
+    return query
+
+def takecommand_listen(listener):
+    with sr.Microphone() as source:
+        print("listening...")
+
+        command = listener.listen(source, phrase_time_limit=5)
+
+    try:
+        query = listener.recognize_google(command, language='es-ES')  # Spanish language configured
+
+    except Exception as e:
         return "None"
 
     return query
@@ -66,6 +80,60 @@ def answer(sentence):
             speak("No le he entendido")
 
     return ans
+
+
+def action(query):
+    if "hola" in query:
+        speak("hola señor")
+    elif "gracias" in query:
+        ans = ["un placer", "no es molestia señor", "De nada señor. Cualquier otra cosa dígamelo"]
+        speak(random.choice(ans))
+
+    elif "hora es" \
+            in query:
+        strTime = datetime.datetime.now().strftime("%H:%M:%S")
+        speak(f"Son las {strTime}, señor")
+
+    elif 'wikipedia' in query:  # if wikipedia found in the query then this block will be executed
+        speak('Buscando en Wikipedia...')
+
+        pos = query.find("wikipedia")
+        query = query[pos + 9:]
+        results = wikipedia.summary(query, sentences=2)
+        speak("Segun Wikipedia")
+        print(results)
+        speak(results)
+    elif "busca" in query and "google" in query:  # if busca and google in the query then this block will be executed
+        pos = query.find("busca")
+        pos2 = query.find("google")
+        query = query[pos + 5:pos2 - 3]  # Ex. "Busca perritos en google"
+        url = "https://www.google.com.tr/search?q={}".format(query)
+        speak("Buascando en internet")
+        webbrowser.open(url)
+    elif "abre" in query and "carpeta" in query:  # Opens folders and files in desktop
+
+        ans = answer("Que carpeta debo abrir señor?")
+
+        filepath = "C:\\Users\\sergi\\Desktop" + "\\" + ans
+        speak(f"Abriendo {ans}")
+        os.startfile(filepath)
+
+        ans = answer("Quiere que abra algún archivo de la carpeta?")
+
+        if ans == "si":
+            ans = answer("Que archivo debo abrir señor?")
+            file = answer(r).lower()
+            try:
+                pos = file.find(".")
+                file1 = file[:pos]
+                file2 = file[pos:]
+                os.startfile(filepath + "\\" + file1 + file2)
+            except:
+                speak("No encuentro el archivo, lo siento")
+        else:
+            speak("De acuerdo, no abriré nada")
+    else:
+        speak('No le he entendido, lo siento')
 
 
 if __name__ == "__main__":
@@ -89,7 +157,7 @@ if __name__ == "__main__":
         listen = True  # Listening command
 
         while listen:  # Karen Listen until user calls her. To call say Karen!
-            sentence = takecommand(r).lower()
+            sentence = takecommand_listen(r).lower()
             if "karen" in sentence:
                 listen = False
 
@@ -97,52 +165,8 @@ if __name__ == "__main__":
 
         query = takecommand(r).lower()  # Karen takes your query
 
-        if "hola" in query:
-            speak("hola señor")
-        elif "gracias" in query:
-            ans = ["un placer", "no es molestia señor", "De nada señor. Cualquier otra cosa dígamelo"]
-            speak(random.choice(ans))
-
-        elif "hora es"\
-                in query:
-            strTime = datetime.datetime.now().strftime("%H:%M:%S")
-            speak(f"Son las {strTime}, señor")
-
-        elif 'wikipedia' in query:  # if wikipedia found in the query then this block will be executed
-            speak('Buscando en Wikipedia...')
-
-            pos = query.find("wikipedia")
-            query = query[pos + 9:]
-            results = wikipedia.summary(query, sentences=2)
-            speak("Segun Wikipedia")
-            print(results)
-            speak(results)
-        elif "busca" in query and "google" in query:  # if busca and google in the query then this block will be executed
-            pos = query.find("busca")
-            pos2 = query.find("google")
-            query = query[pos + 5:pos2 - 3]  # Ex. "Busca perritos en google"
-            url = "https://www.google.com.tr/search?q={}".format(query)
-            speak("Buascando en internet")
-            webbrowser.open(url)
-        elif "abre" in query and "carpeta" in query:  # Opens folders and files in desktop
-
-            ans = answer("Que carpeta debo abrir señor?")
-
-            filepath = "C:\\Users\\sergi\\Desktop" + "\\" + ans
-            speak(f"Abriendo {ans}")
-            os.startfile(filepath)
-
-            ans = answer("Quiere que abra algún archivo de la carpeta?")
-
-            if ans == "si":
-                ans = answer("Que archivo debo abrir señor?")
-                file = answer(r).lower()
-                try:
-                    pos = file.find(".")
-                    file1 = file[:pos]
-                    file2 = file[pos:]
-                    os.startfile(filepath + "\\" + file1 + file2)
-                except:
-                    speak("No encuentro el archivo, lo siento")
-            else:
-                speak("De acuerdo, no abriré nada")
+        try:
+            action(query)
+        except Exception as e:
+            speak('No puedo hacer lo que me ha pedido, lo siento')
+            speak('Cualquier otra cosa, dígamelo')
